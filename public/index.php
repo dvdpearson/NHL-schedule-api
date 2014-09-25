23c613fd28918345089662014c65784b59dc00d0
David Pearson <david.pearson@criagence.ca>
2014-09-25 09:30:01 -0400

    Dependancy injection fix

--- public/index.php
+++ public/index.php
@@ -1,5 +1,5 @@
 <?php
 require_once __DIR__ . '/../vendor/autoload.php';
 
-$api = new \NHLSchedule\Api();
+$api = new \NHLSchedule\Api('20142015');
 $api->dispatch();
--- src/NHLSchedule/api.php
+++ src/NHLSchedule/api.php
@@ -3,8 +3,28 @@ namespace NHLSchedule;
 
 use \Klein\Klein;
 
+/**
+ * Class Api
+ * @package NHLSchedule
+ */
 class Api {
 
+    /**
+     * @var string
+     */
+    private $season;
+
+    /**
+     * @param $season
+     */
+    public function __construct($season)
+    {
+        $this->setSeason($season);
+    }
+
+    /**
+     *
+     */
     public function dispatch()
     {
         // Using klein package for routing
@@ -17,10 +37,40 @@ class Api {
         $klein->dispatch();
     }
 
+    /**
+     * @return mixed
+     */
     private function getGames()
     {
         $xml = new ScheduleImporter();
-        $xml->import();
+
+        if (!file_exists(__DIR__.'\..\..\xml\\'.$this->getSeason().'.xml')) {
+            $xml->import();
+            $xml->saveToFile(__DIR__.'\..\..\xml\\'.$this->getSeason().'.xml');
+        }
+
+        $xml->setXml(simplexml_load_file(__DIR__.'\..\..\xml\\'.$this->getSeason().'.xml'));
+
         return $xml->getXml()->saveXML();
     }
+
+    /**
+     * @return string
+     */
+    public function getSeason()
+    {
+        return $this->season;
+    }
+
+    /**
+     * @param $season
+     * @return $this
+     */
+    public function setSeason($season)
+    {
+        $this->season = $season;
+        return $this;
+    }
+
+
 }
