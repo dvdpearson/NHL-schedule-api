1f2600f3ad25eb7dbfeaa68e7dfd2e85c18c7fcf
David Pearson <p8.david@gmail.com>
2014-09-26 12:26:02 -0400

    Added games selector.. still in progress

--- src/NHLSchedule/api.php
+++ src/NHLSchedule/api.php
@@ -31,7 +31,13 @@ class Api {
         $klein = new Klein();
 
         $klein->respond('GET', '/games', function () {
+            header("Content-Type: application/json");
             echo json_encode(simplexml_load_string($this->getGames()));
+        });
+
+        $klein->respond('GET', '/games/[:team]', function ($request) {
+            header("Content-Type: application/json");
+            echo json_encode($this->getGames($request->team));
         });
 
         $klein->dispatch();
@@ -40,16 +46,20 @@ class Api {
     /**
      * @return mixed
      */
-    private function getGames()
+    private function getGames($team = null)
     {
         $xml = new ScheduleImporter();
 
         if (!file_exists(__DIR__.'\..\..\xml\\'.$this->getSeason().'.xml')) {
-            $xml->import();
+            $xml->import($this->getSeason());
             $xml->saveToFile(__DIR__.'\..\..\xml\\'.$this->getSeason().'.xml');
         }
 
         $xml->setXml(simplexml_load_file(__DIR__.'\..\..\xml\\'.$this->getSeason().'.xml'));
+
+        if ($team !== null) {
+            return $xml->getScheduleByTeam($team);
+        }
 
         return $xml->getXml()->saveXML();
     }
